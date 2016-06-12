153252c7a533cff5b34e690a2fd0c516d5804055
dq7 <dq7@DAN-PC>
2016-06-12 22:42:22 +0100

    Data keyword parser and tests

---
+++ DomainValues.Test/ParsingTests/DataTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainValues.Model;
+using DomainValues.Parsing;
+using NUnit.Framework;
+using static DomainValues.Test.ParsingTests.Util;
+namespace DomainValues.Test.ParsingTests
+{
+    [TestFixture]
+    public class DataTests
+    {
+        [Test]
+        public void DataIsRecognised()
+        {
+            var output = new DataParser().ParseLine(0, "data", TokenType.Data).Single();
+
+            var expectedOutput = new ParsedSpan(0, TokenType.Data, 0, "data");
+
+            AreEqual(expectedOutput,output);
+        }
+
+        [Test]
+        public void DataWithSpacesIsRecognised()
+        {
+            var output = new DataParser().ParseLine(0, " data ", TokenType.Data).Single();
+
+            var expectedOutput = new ParsedSpan(0, TokenType.Data, 1, "data");
+
+            AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void WordStartingWithDataIsNotRegognised()
+        {
+            var output = new DataParser().ParseLine(0, " datatest", TokenType.Data).Single();
+
+            var expectedOutput = new ParsedSpan(0, TokenType.Parameter, 1, "datatest", "Invalid text in file");
+            AreEqual(expectedOutput,output);
+        }
+
+        [Test]
+        public void DataWithParamCreatesInvalid()
+        {
+            var output = new DataParser().ParseLine(0, " data   test  ", TokenType.Data).ToList();
+
+            var expectedOutput = new List<ParsedSpan>
+            {
+                new ParsedSpan(0, TokenType.Data,1, "data"),
+                new ParsedSpan(0, TokenType.Parameter,8, "test", "Data keyword does not have a parameter")
+            };
+
+            AreEqual(expectedOutput, output);
+        }
+
+        [Test]
+        public void NextTokenShouldBeHeaderRow()
+        {
+            var parser = new DataParser();
+
+            var output = parser.ParseLine(0, "data", TokenType.Data).Single();
+
+            Assert.AreEqual(TokenType.HeaderRow,parser.NextTokenType);
+        }
+
+        [Test]
+        public void DataParserPrimaryType()
+        {
+            var parser = new DataParser();
+
+            Assert.AreEqual(TokenType.Data,parser.PrimaryType);
+        }
+    }
+}
---
+++ DomainValues/Parsing/DataParser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using DomainValues.Model;
+using DomainValues.Util;
+
+namespace DomainValues.Parsing
+{
+    internal class DataParser : LineParser
+    {
+        internal override IEnumerable<ParsedSpan> ParseLine(int lineNumber, string source, TokenType? expectedTokenType)
+        {
+            NextTokenType=TokenType.HeaderRow;
+
+            var span = source.GetTextSpan();
+
+            if (!IsValid(span, 4))
+            {
+                yield return new ParsedSpan(lineNumber,TokenType.Parameter, span,"Invalid text in file");
+                yield break;
+            }
+
+            var data = new ParsedSpan(lineNumber,TokenType.Data, span.Start,span.Text.Substring(0,4));
+
+            CheckOrder(data,expectedTokenType);
+
+            yield return data;
+
+            if (span.Text.Length<=4)
+                yield break;
+
+            var param = source.GetTextSpan(span.Start+4);
+
+            if (string.IsNullOrWhiteSpace(param.Text))
+                yield break;
+
+            yield return new ParsedSpan(lineNumber, TokenType.Parameter, param, "Data keyword does not have a parameter");
+        }
+
+        internal override TokenType PrimaryType =>TokenType.Data;
+    }
+}
